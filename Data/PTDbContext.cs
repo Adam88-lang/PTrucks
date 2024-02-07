9e5d740477279f2e9fa823d869bf69621be9449c
adamr <adamr@ADAM_LAPTOP>
2024-02-07 13:41:26 -0600

    full original

---
+++ Data/PTDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PTrucks.Models;
+
+
+namespace PTrucks.Data
+{
+    public class PTDbContext : DbContext
+    {
+        public PTDbContext(DbContextOptions<PTDbContext> options)
+            : base(options)
+        {
+        }   
+
+        public DbSet<DeviceLocation> DeviceLocations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DeviceLocation>().ToTable("Location");
+        }
+
+    }
+}
---
+++ Models/DeviceLocation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace PTrucks.Models
+{
+
+    [Table("Location")]
+    public class DeviceLocation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int LocationID { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string MacAddress { get; set; }
+
+        [Required]
+        [StringLength(5)]
+        public string DeviceRSSI { get; set; }
+
+        [Required]
+        public int DeviceVoltage { get; set; }
+
+        [Required]
+        public int SatelliteCount { get; set; }
+
+        [Required]
+        [StringLength(12)]
+        public string Latitude { get; set; }
+
+        [Required]
+        [StringLength(12)]
+        public string Longitude { get; set; }
+
+        [Required]
+        public int Chassis { get; set; }
+
+        public DateTime? LastHeardDate { get; set; }
+    }
+
+}
